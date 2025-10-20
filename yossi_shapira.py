494bc891b98ae569ebdc386be8ba3d7a261e36d0
yossi shapira <yosi105009@gmail.com>
2025-10-20 17:15:01 +0300

    new

---
+++ yossi_shapira.py
@@ -0,0 +1,57 @@
+# # יוסי שפירא 315545491
+
+# # 1
+# def matrix(size):
+#     for i in range(1,size +1):
+#         for x in range(1,size +1):
+#             print(f"{i * x}", end=" ") 
+#         print()   
+# user_input = int(input("plis enter number: "))
+# matrix(user_input)
+
+# # 2
+# def triangular_number(size):
+#     for i in range(1,size +1):
+#         for x in range(1,i +1):
+#             print(f"{x}",end= " ")
+#         print()
+# user_input = int(input("plis enter number: "))
+# triangular_number(user_input)
+
+# # 3
+# def right_triangle_asterisk(size):
+#     for i in range(1,size +1):
+#             print(f" {"*" * i}")
+# user_input = int(input("plis enter number: "))
+# right_triangle_asterisk(user_input)
+
+# # 4
+# def matrix(size):
+#     for i in range(size):
+#         for x in range( size):
+#             if i == x:
+#                 print(f"{1}",end=" ")
+#             else:
+#                 print(f"{0}",end=" ")
+#         print()
+# user_input = int(input("plis enter number: "))
+# matrix(user_input)
+
+# # 5
+# def inverted_triangle_asterisk(size):
+#     for i in range(size, 0,-1):
+#             print(f" {"*" * i}")
+# user_input = int(input("plis enter number: "))
+# inverted_triangle_asterisk(user_input)
+
+# # 6
+# def matrix(size):
+#     counter = 0 
+#     for i in range(1,size +1):
+#         for x in range(1,size +1):
+#             calculation = i * x  
+#             counter += calculation
+#     print(f"The total sum of the matrix: {counter}")     
+# user_input = int(input("plis enter number: "))
+# matrix(user_input)
+
